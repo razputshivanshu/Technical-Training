8be841615133352ab6b548915aaa54360bd36658
Shivanshu Chauhan <89507255+razputshivanshu@users.noreply.github.com>
2024-02-23 15:21:45 +0530

    Update Experiment_4.cpp

--- Experiment_4.cpp
+++ Experiment_4.cpp
@@ -1,16 +1,16 @@
-//what we have learned already
+
 
 #include<iostream>
 using namespace std;
 
 
-//creating a struct fuction for nodes
+
 struct node{
     int data;
     node *next;
 };
 
-// Print the full linked list from the head or the first element of the linkedlist.
+
 
 void printList(node *n){
     int count=0;
@@ -22,7 +22,7 @@ void printList(node *n){
     cout<<"Number of elements in the Linked List is: "<<count<<endl;
 }
 
-// A function that will insert dynamically in the Linkedlist.
+
 node *push_front(node *head,int newData){
     node *newNode = new node();
     newNode->data=newData;
